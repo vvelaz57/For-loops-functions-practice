ed3e71c3f3857e0e26d7a5a3040d05b2ec88a552
jjhiggz <jonathan.higger@gmail.com>
2023-11-10 11:00:49 -0700

    fixed the description on e13.js

--- exercises/e13.js
+++ exercises/e13.js
@@ -1,15 +1,15 @@
-
 // EXERCISE 13
 // Return an array of bank accounts that have a sum of deposits less than 2000 or no deposits at all
 // Array example: bankAccounts in /data/data.js
-// getAllAccountsWithSumsOfDepositsLess2000(bankAccounts) => [3432, 43242.34, 23432]
+// getAllAccountsWithSumsOfDepositsLess2000(bankAccounts) => [
+// Susan's Account Object,
+// Morgan's Account Object
+// Joshua's Account Object
+// Candy's Account Object,
+// Phil's Account Object
+// ]
 
-export function getAllAccountsWithSumsOfDepositsLess2000(array) {
-  // Your code goes here...
-
-}
-
-
+export function getAllAccountsWithSumsOfDepositsLess2000(array) {}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
